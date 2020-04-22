62f409b9afccab2837dea95dbde3ecacc56e7c83
Bret Barkelew <brbarkel@microsoft.com>
2020-04-22 15:14:39 -0700

    Add separated Allocation, Panic, and Print libs

---
+++ MdeModulePkg/Library/UefiRustPanicLib/src/lib.rs
@@ -0,0 +1,41 @@
+// Copyright (c) 2019 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#![no_std]
+#![feature(lang_items)]
+#![feature(alloc_error_handler)]
+
+#[cfg(not(test))]
+extern crate uefi_rust_allocation_lib;
+#[cfg(not(test))]
+use uefi_rust_print_lib_debug_lib::eprintln;
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+  eprintln!("PANIC!: {:#?}", info);
+  loop {}
+}
+
+#[cfg(not(test))]
+#[alloc_error_handler]
+fn alloc_error_handler(layout: core::alloc::Layout) -> ! {
+    eprintln!("ALLOC ERROR!: {:#?}", layout);
+    loop {}
+}
+
+#[cfg(not(test))]
+#[lang = "eh_personality"]
+#[no_mangle]
+pub extern fn rust_eh_personality() {}
---
+++ MdeModulePkg/Library/UefiRustPrintLibDebugLib/src/lib.rs
@@ -0,0 +1,100 @@
+// @file -- lib.rs
+// Implementation of the Rust print! and println! macros
+// that use the EDK2 DebugLib as the back-end.
+//
+// Copyright (c) Microsoft Corporation.
+// SPDX-License-Identifier: BSD-2-Clause-Patent
+//
+
+// Borrows heavily from Philipp Oppermann's blog on Writing an OS in Rust
+// https://os.phil-opp.com/vga-text-mode/
+
+#![no_std]
+
+extern crate alloc;
+
+use alloc::vec::Vec;
+use core::fmt;
+use r_efi::efi;
+
+extern "C" {
+  fn DebugPrintEnabled () -> efi::Boolean;
+  fn DebugPrintLevelEnabled (Level: usize) -> efi::Boolean;
+  fn DebugPrint(ErrorLevel: usize, Format: *const u8, Arg: ...);
+}
+
+pub const DEBUG_INIT      : usize = 0x00000001;
+pub const DEBUG_WARN      : usize = 0x00000002;
+pub const DEBUG_LOAD      : usize = 0x00000004;
+pub const DEBUG_FS        : usize = 0x00000008;
+pub const DEBUG_POOL      : usize = 0x00000010;
+pub const DEBUG_PAGE      : usize = 0x00000020;
+pub const DEBUG_INFO      : usize = 0x00000040;
+pub const DEBUG_DISPATCH  : usize = 0x00000080;
+pub const DEBUG_VARIABLE  : usize = 0x00000100;
+pub const DEBUG_BM        : usize = 0x00000400;
+pub const DEBUG_BLKIO     : usize = 0x00001000;
+pub const DEBUG_NET       : usize = 0x00004000;
+pub const DEBUG_UNDI      : usize = 0x00010000;
+pub const DEBUG_LOADFILE  : usize = 0x00020000;
+pub const DEBUG_EVENT     : usize = 0x00080000;
+pub const DEBUG_GCD       : usize = 0x00100000;
+pub const DEBUG_CACHE     : usize = 0x00200000;
+pub const DEBUG_VERBOSE   : usize = 0x00400000;
+pub const DEBUG_ERROR     : usize = 0x80000000;
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::_print($crate::DEBUG_INFO, format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! eprint {
+    ($($arg:tt)*) => ($crate::_print($crate::DEBUG_ERROR, format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! eprintln {
+    () => ($crate::eprint!("\n"));
+    ($($arg:tt)*) => ($crate::eprint!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(log_level: usize, args: fmt::Arguments) {
+    use core::fmt::Write;
+    DebugWriter{ log_level: log_level }.write_fmt(args).unwrap();
+}
+
+struct DebugWriter {
+  log_level: usize
+}
+
+impl fmt::Write for DebugWriter {
+  fn write_str(&mut self, s: &str) -> fmt::Result {
+    // Determine whether we're going to do anything.
+    if unsafe { DebugPrintEnabled() } == efi::Boolean::TRUE &&
+        unsafe { DebugPrintLevelEnabled(self.log_level) } == efi::Boolean::TRUE {
+      let mut output_string = s.bytes()
+                              .map(|byte| {
+                                match byte {
+                                    // printable ASCII byte or newline
+                                    0x20..=0x7e | b'\n' => byte,
+                                    // not part of printable ASCII range
+                                    _ => 0xfe,
+                                }
+                              })
+                              .collect::<Vec<u8>>();
+      // We must have a NULL-terminator.
+      output_string.push(0x00);
+
+      unsafe { DebugPrint (self.log_level, output_string.as_ptr()) };
+    }
+    Ok(())
+  }
+}
